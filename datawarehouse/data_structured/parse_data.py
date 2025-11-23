3ada08da513fa0d9c82b628c091f938b1d35e914
lilyung <1079830405@qq.com>
2025-11-23 14:42:55 +0800

    新增数据解析

---
+++ datawarehouse/data_structured/parse_data.py
@@ -0,0 +1,10 @@
+﻿import pandas as pd
+def parse_taobao_data():
+    # 使用原始字符串避免反斜杠转义问题
+    df_taobao = pd.read_csv(r"C:\Users\lilyung\Documents\trae_projects\dataWarehouse\datawarehouse\data_structured\淘宝.txt",
+                            sep='#',lineterminator='\n')
+    # 显示前几行数据
+    print(df_taobao.head())
+
+if __name__ == '__main__':
+    parse_taobao_data()
